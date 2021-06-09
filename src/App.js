a938d6c6b217df2f21dd3be82f48e8b0019c560a
sungmin <pege92@gmail.com>
2021-06-09 20:50:46 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import axios from 'axios'
-import Movie from './Movie';
+import './App.css';
+import { HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-  state = {
-    isLoading:true,
-    movies:[],
-  };
+function App(){
+  return (
+      <HashRouter>
+        <Navigation />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/movie-detail" component={Detail} />
+      </HashRouter>
 
-  getMovies = async () =>{
-    const {
-      data:{
-        data:{ movies},
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading:false});
-  }
-  componentDidMount(){
-    //영화 데이터 로딩!
-    this.getMovies();
-  }
-
-  render(){
-    const { isLoading, movies } = this.state;
-
-    return(
-       <section class="container">
-         {isLoading ?(
-           <div class="loader">
-              <span class = "loader__text">Loading...</span>
-           </div>
-         ) :(
-           <div class="movies">
-             {  movies.map(movie => (
-               <Movie 
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-               />
-             ))}
-            </div>
-          )}
-        </section>
-    );
-  }
+  );
 }
+
 export default App;
